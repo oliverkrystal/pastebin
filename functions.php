774f28face2d4dcc4aea1f3752f50deaa59554ca
Oliver Krystal <oliverk@soliloquyforthefallen.net>
2011-03-26 17:44:57 -0500

    create function getPasteString(), will be used when I redo the paste string method in light of another change (allows revisions)

--- functions.php
+++ functions.php
@@ -1,3 +1,12 @@
-<?php function getPasteString(){
-		echo microtime();
-	}
+<?php
+
+        function getPasteString(){
+                $time = microtime();
+                $string = explode(" ", $time);
+                return $string[1];
+        }
+
+	#Test the functions I've written
+        echo getPasteString();
+
+?>
