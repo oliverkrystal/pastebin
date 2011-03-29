8784e2679bfecb517442dc33abd32c4a1ec1015f
OliverKrystal <oliverk@soliloquyforthefallen.net>
2011-03-28 21:53:09 -0500

    add some randomization to the end of the getPasteString() because microtime is good, but to slow.  3 digits of randomization should fix that.

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
         function getPasteString(){
                 $time = microtime();
                 $string = explode(" ", $time);
-                return $string[1];
+                return $string[1] . (rand()%3);
         }
 
 	#Test the functions I've written
